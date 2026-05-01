7c3ebf19b6e87082566a2b6215dcc75b374cb637
Umidjon Davlatov <umiddavlatov57@gmail.com>
2026-05-01 19:24:58 +0500

    Delete Rore7 directory

--- Rore7/rore-kit/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod image;
-pub mod icon; // <--- YANGI
-
-pub use image::Image;
-pub use icon::Icon;
--- Rore7/rore-layout/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod mapper;
-pub mod tree;
-
-pub use tree::{LayoutEngine, ComputedLayout};
-pub use taffy::node::Node;
